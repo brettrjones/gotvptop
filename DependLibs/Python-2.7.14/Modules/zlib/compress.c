9fe7874b8c59887d5ca3f6a6e1e366a25133c416
Brett Jones <gotvptop@gmail.com>
2020-01-26 14:43:32 -0600

    fix python link error

--- DependLibs/Python-2.7.14/Modules/zlib/compress.c
+++ DependLibs/Python-2.7.14/Modules/zlib/compress.c
@@ -71,7 +71,7 @@ int ZEXPORT compress_python (dest, destLen, source, sourceLen)
     const Bytef *source;
     uLong sourceLen;
 {
-    return compress2(dest, destLen, source, sourceLen, Z_DEFAULT_COMPRESSION);
+    return compress2_python(dest, destLen, source, sourceLen, Z_DEFAULT_COMPRESSION);
 }
 
 /* ===========================================================================
